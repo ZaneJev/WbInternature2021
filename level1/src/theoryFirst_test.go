bd4cd7adf03fd76caff9eff27c247e8d64534f81
Alexandr Atmadzhev <atmadzhev@bk.ru>
2021-08-09 12:40:04 +0300

    benchmarks for first theoryTask

---
+++ level1/src/theoryFirst_test.go
@@ -0,0 +1,67 @@
+package src
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
+
+func BenchmarkConcat(b *testing.B) {
+	var str string
+	for n := 0; n < b.N; n++ {
+		str += "x"
+	}
+	b.StopTimer()
+
+	if s := strings.Repeat("x", b.N); str != s {
+		b.Errorf("unexpected result; got=%s, want=%s", str, s)
+	}
+}
+
+func BenchmarkBuffer(b *testing.B) {
+	var buffer bytes.Buffer
+	for n := 0; n < b.N; n++ {
+		buffer.WriteString("x")
+	}
+	b.StopTimer()
+
+	if s := strings.Repeat("x", b.N); buffer.String() != s {
+		b.Errorf("unexpected result; got=%s, want=%s", buffer.String(), s)
+	}
+}
+
+func BenchmarkCopy(b *testing.B) {
+	bs := make([]byte, b.N)
+	bl := 0
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		bl += copy(bs[bl:], "x")
+	}
+	b.StopTimer()
+
+	if s := strings.Repeat("x", b.N); string(bs) != s {
+		b.Errorf("unexpected result; got=%s, want=%s", string(bs), s)
+	}
+}
+
+func BenchmarkStringBuilder(b *testing.B) {
+	var strBuilder strings.Builder
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		strBuilder.WriteString("x")
+	}
+	b.StopTimer()
+
+	if s := strings.Repeat("x", b.N); strBuilder.String() != s {
+		b.Errorf("unexpected result; got=%s, want=%s", strBuilder.String(), s)
+	}
+}
+
+//my results 9.08.2021
+//BenchmarkConcat-4                 368236             67801 ns/op
+//BenchmarkBuffer-4               69814984                18.6 ns/op
+//BenchmarkCopy-4                 125630400                9.75 ns/op
+//BenchmarkStringBuilder-4        205053986                6.84 ns/op
