14269761c655f323719e8bcc9ea3b7e172a808ab
ZaneJev <atmadzhev@bk.ru>
2021-09-04 18:32:48 +0300

    add 29, clever

--- 29.go
+++ 29.go
@@ -7,40 +7,11 @@ import (
 
 func main(){
 	a, b := new(big.Int), new(big.Int)
+	sum, div, mod, sub, mul := new(big.Int), new(big.Int), new(big.Int), new(big.Int), new(big.Int)
 
 	a.SetString("25000000000000000000", 10)
 	b.SetString("24000000000000000000", 10)
 
 	fmt.Printf("Sum is %v\nDiv is %v\nMod is %v\nSub is %v\nMul is %v\n",
-		Sum(a, b), Div(a, b), Mod(a, b), Sub(a, b), Mul(a, b))
-}
-
-func Sum(a *big.Int, b *big.Int) *big.Int {
-	result := new(big.Int)
-	result.Add(a, b)
-	return result
-}
-
-func Div(a *big.Int, b *big.Int) *big.Int {
-	result := new(big.Int)
-	result.Div(a, b)
-	return result
-}
-
-func Mod(a *big.Int, b *big.Int) *big.Int {
-	result := new(big.Int)
-	result.Mod(a, b)
-	return result
-}
-
-func Sub(a *big.Int, b *big.Int) *big.Int {
-	result := new(big.Int)
-	result.Sub(a, b)
-	return result
-}
-
-func Mul(a *big.Int, b *big.Int) *big.Int {
-	result := new(big.Int)
-	result.Mul(a, b)
-	return result
+		sum.Add(a, b), div.Div(a, b), mod.Mod(a, b), sub.Sub(a, b), mul.Mul(a, b))
 }
