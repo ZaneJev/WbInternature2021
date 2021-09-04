985f803cc6d1515da4b0fa886e5a62e15b9f6115
ZaneJev <atmadzhev@bk.ru>
2021-09-04 15:37:23 +0300

    add 22

---
+++ 22.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func partition(arr []int, low, high int) ([]int, int) {
+	pivot := arr[high]
+	i := low
+	for j := low; j < high; j++ {
+		if arr[j] < pivot {
+			arr[i], arr[j] = arr[j], arr[i]
+			i++
+		}
+	}
+	arr[i], arr[high] = arr[high], arr[i]
+	return arr, i
+}
+
+func quickSort(arr []int, low, high int) []int {
+	if low < high {
+		var p int
+		arr, p = partition(arr, low, high)
+		arr = quickSort(arr, low, p-1)
+		arr = quickSort(arr, p+1, high)
+	}
+	return arr
+}
+
+func main() {
+	a := []int{5, 6, 7, 2, 1, 0}
+	fmt.Println(quickSort(a, 0, len(a)-1))
+}
