0724e12b28fe4eff9c5759e3fb5a62ac0e54af13
ZaneJev <atmadzhev@bk.ru>
2021-09-04 16:21:52 +0300

    fixed

--- 27.go
+++ 27.go
@@ -1,15 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func reverseWordInString(sentence []string) []string {
-	newSentence := make([]string, len(sentence))
-	for i := 0; i < len(sentence); i++ {
-		newSentence[len(sentence)-i-1] = sentence[i]
+func reverseWordInString(sentence string) string {
+	newSentence := strings.Split(sentence, " ")
+
+	for i := 0; i < len(newSentence)/2; i++ {
+		newSentence[i], newSentence[len(newSentence)-i-1] = newSentence[len(newSentence)-i-1], newSentence[i]
 	}
-	return newSentence
+	return strings.Join(newSentence, " ")
 }
 
 func main() {
-	fmt.Println(reverseWordInString([]string{"snow", "dow", "sun"}))
+	fmt.Println(reverseWordInString("snow dow sun"))
 }
